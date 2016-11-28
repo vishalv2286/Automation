7fed22998fa6acc05b4ffab4a35537e5b068782c
vishalv2286 <vishalvishwanath1986@gmail.com>
2016-11-28 14:20:47 +0530

    draft copy

---
+++ src/pom/BasePage.java
@@ -0,0 +1,13 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+public WebDriver driver;
+public BasePage(WebDriver driver)
+{
+	PageFactory.initElements(driver, this);
+	this.driver=driver;
+}
+}
---
+++ src/script/AutoConst.java
@@ -0,0 +1,6 @@
+package script;
+
+public interface AutoConst {
+public static final String XL_PATH="./data/input.xlsx";
+public static final String CD_PATH="./driver/chromedriver.exe";
+}
